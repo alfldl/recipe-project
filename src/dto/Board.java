3575be2e89ecd70a2ba8d58a8604416aa7b9659e
miri <564486_v@naver.com>
2020-03-23 22:25:22 +0900

    board dto add

---
+++ src/dto/Board.java
@@ -0,0 +1,66 @@
+package dto;
+
+import java.util.Date;
+
+public class Board {
+	private int bNo;
+	private int mNo;
+	private String title;
+	private String content;
+	private int reCnt;
+	private int likeCnt;
+	private Date bDate;
+	private int Hits;
+	
+	
+	public int getbNo() {
+		return bNo;
+	}
+	public void setbNo(int bNo) {
+		this.bNo = bNo;
+	}
+	public int getmNo() {
+		return mNo;
+	}
+	public void setmNo(int mNo) {
+		this.mNo = mNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getReCnt() {
+		return reCnt;
+	}
+	public void setReCnt(int reCnt) {
+		this.reCnt = reCnt;
+	}
+	public int getLikeCnt() {
+		return likeCnt;
+	}
+	public void setLikeCnt(int likeCnt) {
+		this.likeCnt = likeCnt;
+	}
+	public Date getbDate() {
+		return bDate;
+	}
+	public void setbDate(Date bDate) {
+		this.bDate = bDate;
+	}
+	public int getHits() {
+		return Hits;
+	}
+	public void setHits(int hits) {
+		Hits = hits;
+	}
+	
+	
+}
