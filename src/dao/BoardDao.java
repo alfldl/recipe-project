cfb8a36fbac6a0c7617641c1d9367cd04dac32d9
alfldl <564486_v@naver.com>
2020-03-24 14:28:44 +0900

    err

--- src/dao/BoardDao.java
+++ src/dao/BoardDao.java
@@ -1,5 +1,50 @@
 package dao;
 
-public class BoardDao {
+import java.sql.*;
 
+import javax.naming.*;
+import javax.sql.DataSource;
+
+
+
+
+
+public class BoardDao {
+	
+	private BoardDao() {}
+	
+	private static BoardDao instance;
+	public static BoardDao getInstance() {
+		if (instance == null) {
+			instance = new BoardDao();
+		}
+		return  instance;
+	}
+	
+	private Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/OracleDB");
+			conn = ds.getConnection(); 
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+	
+//	public int getTotalCnt() throws SQLException {
+//		
+//	}
+	
 }
+
+
+
+
+
+
+
+
+
+
--- src/service/BoardListAction.java
+++ src/service/BoardListAction.java
@@ -6,13 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeAction implements CommandProcess {
+public class BoardListAction implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("homeAction Start...");
-		return "main/home.jsp";
+		
+		return "boardlist.jsp";
 	}
 
 }
